5d59d2dc8db3e8480b3069593a1bcfe6e50c4072
Lakshya Rao <lakshyarao22@gmail.com>
2022-07-23 18:53:02 +0530

    VSCODE THEME

--- init.lua
+++ init.lua
@@ -6,4 +6,4 @@ require "user.tabline"
 require "user.colorscheme"
 require "user.cmp"
 require "user.lsp"
-
+require "user.vscode"
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,6 +44,7 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
   use 'folke/tokyonight.nvim' -- Tokyonight Theme
+  use 'Mofiqul/vscode.nvim' --vscode Theme
   
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
